59be3b625a8b167235afb5b257115abe67ac81b9
Kyle-Myre <simoarmymen9@gmail.com>
2024-11-08 00:05:01 +0100

    Added dark theme

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,34 @@
 import { RxCross2 } from "react-icons/rx";
 import { FaBars } from "react-icons/fa";
-import React from "react";
+import { IoIosSunny as Light } from "react-icons/io";
+import { IoMdMoon as Dark } from "react-icons/io";
+
+import { useState , useEffect } from "react";
 
 export default function Navbar() {
-  const [visible, setVisible] = React.useState(false);
+  
+  const [visible, setVisible] = useState(false);
+
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "lemonade"
+  );
+
+  const handleToggle = (e) => {
+    if (e.target.checked) {
+      setTheme("forest");
+    } else {
+      setTheme("lemonade");
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    const localTheme = localStorage.getItem("theme");
+    document
+      .querySelector("html")
+      .setAttribute("data-theme", localTheme);
+  }, [theme]);
+
   const links = [
     { name: "About", href: "#about" },
     { name: "Skills", href: "#skills" },
@@ -15,12 +40,16 @@ export default function Navbar() {
     <header className="container font-monsterate">
       <nav className="my-10 flex flex-row items-center">
         <section className="flex-1">
-          <a
-            className="text-3xl hover:text-primary transition-all duration-500"
-            href="#"
-          >
-            Kyle Myre
-          </a>
+          <label className="swap swap-rotate">
+            <input
+              type="checkbox"
+              className="theme-controller"
+              checked={theme === "lemonade" ? false : true}
+              onChange={handleToggle}
+            />
+            <Dark className="size-7  swap-on" />
+            <Light className="size-7 swap-off" />
+          </label>
         </section>
         <section className="flex-2 lg:flex hidden">
           {links.map((link, key) => (
@@ -36,11 +65,13 @@ export default function Navbar() {
 
         <section className="flex align-center gap-1">
           <button className="lg:hidden" onClick={() => setVisible(!visible)}>
-            {!visible ? <FaBars className="size-7" /> : <RxCross2 className="size-8" />}
+            {!visible ? (
+              <FaBars className="size-7" />
+            ) : (
+              <RxCross2 className="size-8" />
+            )}
           </button>
-          
         </section>
-
       </nav>
       {visible ? (
         <section className="flex flex-col justify-center lg:hidden">

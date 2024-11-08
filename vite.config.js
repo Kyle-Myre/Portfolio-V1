152a155e6bb6e75754aa693828d20e76ebc3549a
Kyle-Myre <simoarmymen9@gmail.com>
2024-11-08 13:04:53 +0100

    Added gh-pages to pnpm

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/main',
-  build : {
-    outDir : 'dist'
-  }
+  base : '/main/',
 })
